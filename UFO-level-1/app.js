3df43b7f71aaf12e88195cb089ba55b9ee6388f5
nadiarichards <73898387+nadiarichards@users.noreply.github.com>
2021-03-02 23:05:06 -0500

    map the data

--- UFO-level-1/app.js
+++ UFO-level-1/app.js
@@ -1,6 +1,24 @@
 // from data.js
 var tableData = data;
 
+
+d3.json(url).then(function(data) {
+
+    // @TODO: Grab Name, Stock, Start Date, and End Date from the response json object to build the plots
+    var name = data.dataset.name;
+    var stock = data.dataset.dataset_code;
+    var startDate = data.dataset.start_date;
+    var endDate = data.dataset.end_date;
+    
+    // @TODO: Unpack the dates, open, high, low, and close prices
+    var dates = unpack(data.dataset.data, 0);
+    var openingPrices = unpack(data.dataset.data, 1);
+    var highPrices = unpack(data.dataset.data, 2);
+    var lowPrices = unpack(data.dataset.data, 3);
+    var closingPrices = unpack(data.dataset.data, 4);
+
+    getMonthlyData();
+
 // YOUR CODE HERE!
 function buildTable(dates, openPrices, highPrices, lowPrices, closingPrices, volume) {
     var table = d3.select("#summary-table");
