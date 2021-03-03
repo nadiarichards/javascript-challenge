3e0dd4882eaf80558b37dee04a56281a002f66d4
nadiarichards <73898387+nadiarichards@users.noreply.github.com>
2021-03-02 23:01:26 -0500

    table skeleton

--- UFO-level-1/app.js
+++ UFO-level-1/app.js
@@ -2,3 +2,17 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+function buildTable(dates, openPrices, highPrices, lowPrices, closingPrices, volume) {
+    var table = d3.select("#summary-table");
+    var tbody = table.select("tbody");
+    var trow;
+    for (var i = 0; i < 12; i++) {
+      trow = tbody.append("tr");
+      trow.append("td").text(dates[i]);
+      trow.append("td").text(openPrices[i]);
+      trow.append("td").text(highPrices[i]);
+      trow.append("td").text(lowPrices[i]);
+      trow.append("td").text(closingPrices[i]);
+      trow.append("td").text(volume[i]);
+    }
+  }
