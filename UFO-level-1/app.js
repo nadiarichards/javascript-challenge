b2457f9ac442d66b7c4c49242038e6b864f2d149
nadiarichards <73898387+nadiarichards@users.noreply.github.com>
2021-03-02 23:13:23 -0500

    test code

--- UFO-level-1/app.js
+++ UFO-level-1/app.js
@@ -40,3 +40,55 @@ function buildTable(dates, openPrices, highPrices, lowPrices, closingPrices, vol
       trow.append("td").text(volume[i]);
     }
   }
+
+
+  var names = students.map(function(student) {
+    return student.name;
+  });
+  
+  var scores = students.map(function(student) {
+    return student.score;
+  });
+  
+  // Map vs forEach
+  // Part A
+  var forEachStages = theStagesOfJS.forEach(function(each, index) {
+    // the return of forEach is ignored
+    return `Stage ${index + 1}: ${each}`;
+  });
+  
+  // undefined
+  console.log(forEachStages);
+  
+  // unaltered
+  console.log(theStagesOfJS);
+  
+  // Part B
+  theStagesOfJS.forEach(function(each, index) {
+    // The original array is mutated with forEach
+    theStagesOfJS[index] = `Stage ${index + 1}: ${each}`;
+  });
+  
+  // Note that the original array has been altered (mutated)
+  console.log(theStagesOfJS);
+  
+  // Challenge Activity!
+  var princesses = [
+    { name: "Rapunzel", age: 18 },
+    { name: "Mulan", age: 16 },
+    { name: "Anna", age: 18 },
+    { name: "Moana", age: 16 }
+  ];
+  
+  // Log the name of each princess, follow by a colon, followed by their age
+  // forEach: executes a provided function once for each array element
+  princesses.forEach(function(princess) {
+    console.log(`${princess.name}: ${princess.age}`);
+  });
+  
+  // Create an array of just the names from the princesses array
+  // map: creates a new array with the results of calling a provided function on every element in the calling array
+  var names = princesses.map(function(princess) {
+    return princess.name;
+  });
+  console.log("names: ", names);
