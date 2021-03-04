5cb8953895b50754df00e79051f97e9a02c12dcf
nadiarichards <73898387+nadiarichards@users.noreply.github.com>
2021-03-04 07:05:30 -0500

    update

--- UFO-level-1/app.js
+++ UFO-level-1/app.js
@@ -1,6 +1,19 @@
 // from data.js
 var tableData = data;
 
+function unpack(rows, index) {
+    return rows.map(function(row) {
+      return row[index];
+    });
+  }
+
+reversedData = tabledata.map(object => object.greekSearchResults);
+reversedData.map(object => object.greekSearchResults)
+
+function init() {
+
+}
+
 function handleSubmit() {
     // Prevent the page from refreshing
     d3.event.preventDefault();
@@ -8,17 +21,13 @@ function handleSubmit() {
   console.log(stock);
 
   // clear the input value
-  d3.select("#stockInput").node().value = "";
+  d3.select("#datetime").node().value = "";
 
   // Build the plot with the new stock
   buildPlot(stock);
 }
 
-function unpack(rows, index) {
-    return rows.map(function(row) {
-      return row[index];
-    });
-  }
+
 
 
 d3.json(url).then(function(data) {
